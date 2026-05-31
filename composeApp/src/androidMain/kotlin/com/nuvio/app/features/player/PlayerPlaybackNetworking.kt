f357c3229ae7d207c8d0c09d6498a86cc5d83f45
cyberalby2 <enigmalspd1@gmail.com>
2026-05-31 18:31:25 +0200

    fix mixdrop ( @realbestia1 )

--- composeApp/src/androidMain/kotlin/com/nuvio/app/features/player/PlayerPlaybackNetworking.kt
+++ composeApp/src/androidMain/kotlin/com/nuvio/app/features/player/PlayerPlaybackNetworking.kt
@@ -61,9 +61,12 @@ internal object PlayerPlaybackNetworking {
 
     fun createHttpDataSourceFactory(defaultHeaders: Map<String, String> = emptyMap()): DataSource.Factory {
         val mergedHeaders = DEFAULT_STREAM_HEADERS + defaultHeaders
+        
         return OkHttpDataSource.Factory(playbackHttpClient).apply {
-            setDefaultRequestProperties(mergedHeaders)
-            setUserAgent(DEFAULT_USER_AGENT)
+        setDefaultRequestProperties(mergedHeaders)
+            if (defaultHeaders.none { it.key.equals("User-Agent", ignoreCase = true) }) {
+                setUserAgent(DEFAULT_USER_AGENT)
+            }
         }
     }
 
