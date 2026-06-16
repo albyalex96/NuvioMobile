c294be3d2c55c31edd0ac6500cf6a7e8247612e5
albyalex96 <dragonballaf2007@gmail.com>
2026-06-16 17:18:17 +0200

    fix svg icons

--- composeApp/src/androidMain/kotlin/com/nuvio/app/features/settings/IntegrationLogoPainter.android.kt
+++ composeApp/src/androidMain/kotlin/com/nuvio/app/features/settings/IntegrationLogoPainter.android.kt
@@ -5,14 +5,8 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import com.nuvio.app.R
 import nuvio.composeapp.generated.resources.Res
-import nuvio.composeapp.generated.resources.anilist_logo
 import nuvio.composeapp.generated.resources.introdb_favicon
-import nuvio.composeapp.generated.resources.kitsu_logo
-import nuvio.composeapp.generated.resources.myanimelist_logo
-import nuvio.composeapp.generated.resources.opensubtitles_favicon
 import nuvio.composeapp.generated.resources.rating_tmdb
-import nuvio.composeapp.generated.resources.simkl_logo
-import nuvio.composeapp.generated.resources.subdl_favicon
 import org.jetbrains.compose.resources.painterResource as composePainterResource
 
 @Composable
@@ -22,10 +16,10 @@ internal actual fun integrationLogoPainter(logo: IntegrationLogo): Painter =
         IntegrationLogo.Trakt -> painterResource(id = R.drawable.trakt_tv_favicon)
         IntegrationLogo.MdbList -> painterResource(id = R.drawable.mdblist_logo)
         IntegrationLogo.IntroDb -> composePainterResource(Res.drawable.introdb_favicon)
-        IntegrationLogo.Mal -> composePainterResource(Res.drawable.myanimelist_logo)
-        IntegrationLogo.Kitsu -> composePainterResource(Res.drawable.kitsu_logo)
-        IntegrationLogo.Anilist -> composePainterResource(Res.drawable.anilist_logo)
-        IntegrationLogo.Simkl -> composePainterResource(Res.drawable.simkl_logo)
-        IntegrationLogo.OpenSubtitles -> composePainterResource(Res.drawable.opensubtitles_favicon)
-        IntegrationLogo.Subdl -> composePainterResource(Res.drawable.subdl_favicon)
+        IntegrationLogo.Mal -> painterResource(id = R.drawable.myanimelist_logo)
+        IntegrationLogo.Kitsu -> painterResource(id = R.drawable.kitsu_logo)
+        IntegrationLogo.Anilist -> painterResource(id = R.drawable.anilist_logo)
+        IntegrationLogo.Simkl -> painterResource(id = R.drawable.simkl_logo)
+        IntegrationLogo.OpenSubtitles -> painterResource(id = R.drawable.opensubtitles_favicon)
+        IntegrationLogo.Subdl -> painterResource(id = R.drawable.subdl_favicon)
     }
