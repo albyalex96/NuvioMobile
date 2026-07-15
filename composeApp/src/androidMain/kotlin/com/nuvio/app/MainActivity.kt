f6f5b7a6e6b1164d9a0b3862ec990a81c3ee26ca
albyalex96 <albyalex96@users.noreply.github.com>
2026-07-15 12:16:39 +0200

    initialize cloudstream storage

--- composeApp/src/androidMain/kotlin/com/nuvio/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/nuvio/app/MainActivity.kt
@@ -15,6 +15,7 @@ import com.nuvio.app.core.deeplink.handleAppUrl
 import com.nuvio.app.core.storage.PlatformLocalAccountDataCleaner
 import com.nuvio.app.core.sync.SyncClientIdentityStorage
 import com.nuvio.app.features.addons.AddonStorage
+import com.nuvio.app.features.cloudstream.CloudStreamPlatformStorage
 import com.nuvio.app.features.collection.CollectionMobileSettingsStorage
 import com.nuvio.app.features.collection.CollectionStorage
 import com.nuvio.app.features.debrid.DebridSettingsStorage
@@ -123,6 +124,7 @@ class MainActivity : AppCompatActivity() {
         StreamBadgeSettingsStorage.initialize(applicationContext)
         BingeGroupCacheStorage.initialize(applicationContext)
         PluginStorage.initialize(applicationContext)
+        CloudStreamPlatformStorage.initialize(applicationContext)
         CollectionMobileSettingsStorage.initialize(applicationContext)
         CollectionStorage.initialize(applicationContext)
         DownloadsStorage.initialize(applicationContext)
